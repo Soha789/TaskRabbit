f3bf63e9b72fe58618148044a618b820b40b404c
Soha <sohasalehzain@gmail.com>
2025-10-29 18:46:01 +0300

    Create logout.php

---
+++ logout.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Logout</title>
+<script>
+alert("You have been logged out successfully!");
+window.location = "index.php";
+</script>
+</head>
+<body></body>
+</html>
